dcb751d2263435b425ed51867c73e2f2f7b8d590
Meina <darkstarmeina@gmail.com>
2026-03-22 09:39:19 -0300

    refactor: Adicionado SellerResponseDTO para evitar jackson serialization na entidade Seller, ajustada as configurações no application.properties

--- sales-api/src/main/java/com/dev/sales_api/services/SellerService.java
+++ sales-api/src/main/java/com/dev/sales_api/services/SellerService.java
@@ -1,5 +1,6 @@
 package com.dev.sales_api.services;
 
+import com.dev.sales_api.dtos.SellerResponseDTO;
 import com.dev.sales_api.models.Seller;
 import com.dev.sales_api.repositories.SellerRepository;
 
@@ -19,12 +20,16 @@ public class SellerService {
         this.sellerRepository = sellerRepository;
     }
 
-    public List<Seller> findAll() {
-        return sellerRepository.findAll();
+    public List<SellerResponseDTO> findAll() {
+    return sellerRepository.findAll()
+            .stream()
+            .map(s -> new SellerResponseDTO(s.getId(), s.getName()))
+            .collect(java.util.stream.Collectors.toList());
     }
 
-    public Seller findById(Long id) {
-        return sellerRepository.findById(id)
+    public SellerResponseDTO findById(Long id) {
+        Seller seller = sellerRepository.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Vendedor não encontrado com o ID: " + id));
+        return new SellerResponseDTO(seller.getId(), seller.getName());
     }
 }
